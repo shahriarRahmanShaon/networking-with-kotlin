dc2be676db192e33e819336b10e377e0d5d626f7
shahriarRahmanShaon <shaon463@gmail.com>
2021-12-12 23:22:58 +0600

    image showcasing locally

--- app/src/main/java/com/example/myapplication/home_view.kt
+++ app/src/main/java/com/example/myapplication/home_view.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import org.w3c.dom.Text
 
 class home_view : AppCompatActivity() {
@@ -16,6 +19,9 @@ class home_view : AppCompatActivity() {
 
         var textPlace = findViewById<TextView>(R.id.textView)
         textPlace.setText(intent.getStringExtra("gotFromInternet"))
+
+        var imageView = findViewById<ImageView>(R.id.imageView)
+        Glide.with(this).load("https://picsum.photos/200/300").into(imageView)
     }
 
     override fun onSupportNavigateUp(): Boolean {
